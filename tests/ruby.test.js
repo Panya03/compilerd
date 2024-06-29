2ad548cc2669641264babf9316c9fb8ce9b03af7
Panya <103807205+Panya03@users.noreply.github.com>
2024-06-29 23:16:46 +0530

    Update ruby.test.js

--- tests/ruby.test.js
+++ tests/ruby.test.js
@@ -1,20 +1,15 @@
-
-const { exec } = require('child_process');
-const fs = require('fs');
-
-describe('Ruby Compiler Tests', () => {
-    test('Ruby Compilation and Execution', (done) => {
-        const code = `
-            puts "Hello, World!"
-        `;
-        fs.writeFileSync('solution.rb', code);
-        exec('ruby solution.rb', (error, stdout, stderr) => {
-            if (error) {
-                done(error);
-                return;
-            }
-            expect(stdout.trim()).toBe('Hello, World!');
-            done();
-        });
-    });
-});
+{
+    name: 'ruby : hello world',
+    reqObject: {
+        code: `
+            puts 'Hello, World!'
+        `,
+        language: 'ruby',
+        input: ''
+    },
+    expectedResponse: {
+        status: 200,
+        error: null,
+        val: 'Hello, World!\n' 
+    }
+}
