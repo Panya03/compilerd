3ba5b96de70e508acc1bc723a732b1423a3b4432
Panya <103807205+Panya03@users.noreply.github.com>
2024-06-29 23:17:36 +0530

    Update java.test.js

--- tests/java.test.js
+++ tests/java.test.js
@@ -1,24 +1,20 @@
 
-const { exec } = require('child_process');
-const fs = require('fs');
-
-describe('Java Compiler Tests', () => {
-    test('Java Compilation and Execution', (done) => {
-        const code = `
-            public class Solution {
+{
+    name: 'java : hello world',
+    reqObject: {
+        code: `
+            public class HelloWorld {
                 public static void main(String[] args) {
                     System.out.println("Hello, World!");
                 }
             }
-        `;
-        fs.writeFileSync('Solution.java', code);
-        exec('javac Solution.java && java Solution', (error, stdout, stderr) => {
-            if (error) {
-                done(error);
-                return;
-            }
-            expect(stdout.trim()).toBe('Hello, World!');
-            done();
-        });
-    });
-});
+        `,
+        language: 'java',
+        input: ''
+    },
+    expectedResponse: {
+        status: 200,
+        error: null,
+        val: 'Hello, World!\n' 
+    }
+}
