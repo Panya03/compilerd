a3a8178ced8d1916a3a6cd08ec35aa52f0b0c21a
Panya <103807205+Panya03@users.noreply.github.com>
2024-06-30 11:31:18 +0530

    Update java.test.js

--- tests/java.test.js
+++ tests/java.test.js
@@ -1,20 +1,5 @@
-
-{
-    name: 'java : hello world',
-    reqObject: {
-        code: `
-            public class HelloWorld {
-                public static void main(String[] args) {
-                    System.out.println("Hello, World!");
-                }
-            }
-        `,
-        language: 'java',
-        input: ''
-    },
-    expectedResponse: {
-        status: 200,
-        error: null,
-        val: 'Hello, World!\n' 
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
     }
 }
