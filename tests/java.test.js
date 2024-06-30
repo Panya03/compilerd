856067af19d1f1532746d035a65a437e46a9b0aa
Panya <103807205+Panya03@users.noreply.github.com>
2024-06-30 11:51:40 +0530

    Update java.test.js

--- tests/java.test.js
+++ tests/java.test.js
@@ -1,5 +1,19 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
+const testCase = {
+  name: "java : hello world",
+  reqObject: {
+    language: "java",
+    code: `public class Main {
+        public static void main(String[] args) {
+            System.out.println("Hello, World!");
+        }
+    }`,
+    input: ""
+  },
+  expectedResponse: {
+    val: "Hello, World!",
+    status: 200,
+    error: null
+  }
+};
+
+module.exports = testCase;
